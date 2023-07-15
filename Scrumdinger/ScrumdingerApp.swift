606ef26d8e4e51963b48516a3dd48c6422f418c7
Lenabalakumar Subbarayan <lenabalakumar@gmail.com>
2023-07-15 12:49:23 +0100

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Lenabalakumar Subbarayan on 15/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
